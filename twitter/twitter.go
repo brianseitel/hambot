8118ecb041c778af972551107ba492c79331d019
Brian Seitel <brianseitel@gmail.com>
2018-02-06 15:13:07 -0800

    Update twitter.go

--- twitter/twitter.go
+++ twitter/twitter.go
@@ -13,8 +13,6 @@ func init() {
 
 	config := oauth1.NewConfig(appConfig.ConsumerToken, appConfig.ConsumerSecret)
 	token := oauth1.NewToken(appConfig.AccessToken, appConfig.AccessSecret)
-	// config := oauth1.NewConfig("6lHa4wdDL3Nt7Ijd4DAQkP627", "NoBFqKhoAMMonF3BTxvxjm1ckhiHF9A9tlaDuA6ejmMtI9k154")
-	// token := oauth1.NewToken("960391604170207234-Ta3bAgH5gK8jf8EyeieNFl5XkRL6m62", "TdUoLU7PKy92NXU4ndd5HnGsmL0Ia95JfmorB4omPMubf")
 	httpClient := config.Client(oauth1.NoContext, token)
 
 	// Twitter client
